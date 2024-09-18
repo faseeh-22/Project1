0741d02d66c1e2e032035f963541730caf3ce505
faseeh-22 <130156388+faseeh-22@users.noreply.github.com>
2024-09-18 19:48:23 +0500

    Update final1.sh

--- final1.sh
+++ final1.sh
@@ -1,19 +1,13 @@
-
 #!/bin/bash
+
 filename=$1
-if [ ! -f "$filename" ]; then
-    echo "File $filename not found."
-    exit 1
-fi
-echo -n "Enter the replacement word for 'Product_name': "
-read -r product
-echo -n "Enter the replacement word for 'Endpoint_name' with 1st alphabet capital: "
-read -r endpoint
-echo -n "Enter the replacement word for 'Request_uri': "
-read -r request_uri
-echo -n "Enter the replacement word for 'Index-pattern': "
-read -r index
+product=$2
+endpoint=$3
+request_uri=$4
+index=$5
+
 mapfile -t client_urls < domain_file
+
 for ((i=1; i<${#client_urls[@]}; i++)); do
     if grep -q "DOMAIN$i\b" "$filename"; then
         sed -i "s/DOMAIN$i\b/${client_urls[i]}/g" "$filename"
@@ -21,8 +15,11 @@ for ((i=1; i<${#client_urls[@]}; i++)); do
         echo "Error: 'DOMAIN$i' not found in $filename."
     fi
 done
+cat domain_file
+
 sed -i "s/PRODUCT/$product/g" "$filename"
 sed -i "s/ENDPOINT/$endpoint/g" "$filename"
 sed -i "s/REQ_URI/$request_uri/g" "$filename"
 sed -i "s/INDEX/$index/g" "$filename"
+
 echo "Replacements complete in $filename."
